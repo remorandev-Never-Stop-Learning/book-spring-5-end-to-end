821c492da7349d99603330a24e6f07420315dafa
Roberto Moran <remorandev@gmail.com>
2020-05-31 17:05:42 +0200

    Creacion de CategoryService
    Simulación de base para el servicio  CategoryService

---
+++ cms/src/main/java/es/spring5/remoran/cms/domain/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package es.spring5.remoran.cms.domain.repository;
+
+import es.spring5.remoran.cms.domain.model.Category;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryRepository extends AbstractRepository<Category> {
+
+}
