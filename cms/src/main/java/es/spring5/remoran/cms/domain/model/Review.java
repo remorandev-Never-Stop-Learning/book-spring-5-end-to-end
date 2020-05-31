a6add5e5a70798cbf68d7215771222cfb927fad6
Roberto Moran <remorandev@gmail.com>
2020-05-31 02:37:06 +0200

    Estructura de clase News

--- cms/src/main/java/es/spring5/remoran/cms/domain/model/Review.java
+++ cms/src/main/java/es/spring5/remoran/cms/domain/model/Review.java
@@ -1,4 +1,13 @@
 package es.spring5.remoran.cms.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Review {
+    String userId;
+    String status;
 }
