d59c6487eed5edd9d132cd1145ea7768d5847a11
Roberto Moran <remorandev@gmail.com>
2020-06-06 00:13:00 +0200

    Estructura de servicio UserService
    Se agrego la clase para exponer como servicio  de acceso a la base de datos UserService y UserRepository

---
+++ cms/src/main/java/es/spring5/remoran/cms/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package es.spring5.remoran.cms.domain.repository;
+
+import es.spring5.remoran.cms.domain.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRepository extends AbstractRepository<User> {
+}
---
+++ cms/src/main/java/es/spring5/remoran/cms/domain/service/UserService.java
@@ -0,0 +1,48 @@
+package es.spring5.remoran.cms.domain.service;
+
+import es.spring5.remoran.cms.domain.model.User;
+import es.spring5.remoran.cms.domain.repository.UserRepository;
+import es.spring5.remoran.cms.domain.vo.UserRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User update(String id, UserRequest userRequest) {
+        final User user = this.userRepository.findOne(id);
+        user.setIdentity(userRequest.getIdentity());
+        user.setName(userRequest.getName());
+        user.setRole(userRequest.getRole());
+        return this.userRepository.save(user);
+    }
+
+    public User create(UserRequest userRequest) {
+        User user = new User();
+        user.setId(UUID.randomUUID().toString());
+        user.setIdentity(userRequest.getIdentity());
+        user.setName(userRequest.getName());
+        user.setRole(userRequest.getRole());
+        return userRepository.save(user);
+    }
+
+    public void delete(String id) {
+        final User user = this.userRepository.findOne(id);
+        this.userRepository.delete(user);
+    }
+
+    public List<User> findAll(){
+        return this.userRepository.findAll();
+    }
+
+    public User findOne(String id){
+        return this.userRepository.findOne(id);
+    }
+}
